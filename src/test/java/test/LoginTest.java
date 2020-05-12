83f6bf176e7ed39e59d9d71fac9606d9ee72a6ce
tnthuy <thuy140583@gmail.com>
2020-05-12 16:53:20 +0700

    update1

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -1,4 +1,6 @@
-package com.pop.test;
+package test;
+
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,34 +8,35 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.pop.page.Hompage;
-import com.pop.page.Login;
-import com.pop.page.MyAccount;
+import page.Hompage;
+import page.Login;
+import page.MyAccount;
 
 public class LoginTest {
 	WebDriver driver;
-  @Test
-  public void testLogin() {
-	  
-	  Hompage home=new Hompage(driver);
-	  Login login= home.clickSignIn();
-	  MyAccount myAcc=login.login();
-	  home=myAcc.back2Homepage();
-	  assert (home.isElementPresent(home.LNK_BESTSELLER));
-	  
-	  
-  }
-  @BeforeMethod
-  public void beforeMethod() {
-	  System.setProperty("webdriver.chrome.driver", "E://Setup/chromedriver_win32/chromedriver.exe");
+	@Test
+	public void testLogin() {
+
+		Hompage home=new Hompage(driver);
+		Login login= home.clickSignIn();
+		MyAccount myAcc=login.login();
+		home=myAcc.back2Homepage();
+		assert (home.isElementPresent(home.LNK_BESTSELLER));
+
+
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+		System.setProperty("webdriver.chrome.driver", "E://Setup/chromedriver_win32/chromedriver.exe");
 		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("http://automationpractice.com/");
-  }
+	}
 
-  @AfterMethod
-  public void afterMethod() {
-	  driver.close();
-  }
+	@AfterMethod
+	public void afterMethod() {
+		//driver.close();
+	}
 
 }
