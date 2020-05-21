073385d978e37e17b5b6f1e1e2ab69131122a1a3
tnthuy <thuy140583@gmail.com>
2020-05-21 18:16:53 +0700

    ff

---
+++ src/test/java/page/CreateAccount.java
@@ -0,0 +1,13 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class CreateAccount extends Base {
+	WebDriver driver;
+public CreateAccount(WebDriver driver) {
+	this.driver=driver;
+}
+  public Confirm createAccount(String email) {
+  }
+}
