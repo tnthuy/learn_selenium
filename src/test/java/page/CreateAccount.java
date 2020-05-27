39bd566108074e047c94325de80270cadb42a48c
thuytn <thuy140583@gmail.com>
2020-05-27 22:27:33 +0700

    rr

--- src/test/java/page/CreateAccount.java
+++ src/test/java/page/CreateAccount.java
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-package page;
 
-import org.openqa.selenium.WebDriver;
 
-public class CreateAccount extends Base {
-	WebDriver driver;
-public CreateAccount (WebDriver driver) {
-	this.driver=driver;
-}
-}
-=======
-=======
->>>>>>> 073385d978e37e17b5b6f1e1e2ab69131122a1a3
 package page;
 
 import org.openqa.selenium.WebDriver;
@@ -23,10 +10,6 @@ public class CreateAccount extends Base {
 public CreateAccount(WebDriver driver) {
 	this.driver=driver;
 }
-  public Confirm createAccount(String email) {
-  }
+  
 }
-<<<<<<< HEAD
->>>>>>> 073385d978e37e17b5b6f1e1e2ab69131122a1a3
-=======
->>>>>>> 073385d978e37e17b5b6f1e1e2ab69131122a1a3
+
