788a57a6430376901e6b636a53fb968cb530f41d
tnthuy <thuytn@emotiv.vn>
2020-06-18 06:14:46 +0700

    git push :wq

--- src/test/java/page/LoginGmail.java
+++ src/test/java/page/LoginGmail.java
@@ -1,5 +1,6 @@
 package page;
 
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -14,30 +15,46 @@ public class LoginGmail extends Base {
 	public By BTN_NEXTEMAIL=By.xpath("//*[@id=\"identifierNext\"]");
 	public By TXT_PASSWORD=By.name("password");
 	public By BTN_NEXT=By.xpath("//*[@id=\"passwordNext\"]");
-	public By BTN_INBOX=By.xpath("//*[@id=\":ln\"]");
+	public By BTN_INBOX=By.xpath("//*[@id=\":3a\"]");
 	
 	public LoginGmail(WebDriver driver) {
 		// TODO Auto-generated constructor stub
 		this.driver=driver;
 	}
-	public void LoginGmail() {
+
+	public void LoginGmail() throws Exception {
+		//Mo Gmail
 		System.setProperty("webdriver.chrome.driver", "C://Setup/chromedriver_win32/chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com.vn/");
+		//Dang nhap
 		WebElement DangNhap=driver.findElement(BTN_DANGNHAP);
 		DangNhap.click();
-		driver.findElement(TXT_EMAIL).sendKeys("ngocthuy140583@gmail.com");
+		driver.findElement(TXT_EMAIL).sendKeys("tnthuy140583@gmail.com");
 		driver.findElement(BTN_NEXTEMAIL).click();
 		driver.findElement(TXT_PASSWORD).sendKeys("Thuyvy12");
 		driver.findElement(BTN_NEXT).click();
-		CheckMail("automationpractice.com");
+		driver.findElement(By.xpath("//*[contains(text(), 'Gmail')]")).click();
+		CheckMail("automationpractice");
 	}
-	public void CheckMail(String company) {
+	public void CheckMail(String company) throws Exception {
+		//Click Inbox
 		driver.findElement(BTN_INBOX).click();
-		Properties props=System.getProperties();
-	System.out.println("vua click Inbox");
+		List<WebElement> unreadmail=(List<WebElement>) driver.findElement(By.className("zE"));
+		System.out.println("Total No. of unread mail"+unreadmail.size());
+		//real logic start here
+		for (int i=0;i<=unreadmail.size();i++) {
+			System.out.println(unreadmail.get(i).getText());
+			if (unreadmail.get(i).getText()==company) {
+				System.out.println("Da gui mail");
+			}
+			
+		}
+		unreadmail.get(0).click();
+		Thread.sleep(3000);
+		driver.close();
 	}
 
 }
