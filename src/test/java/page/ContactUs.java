ba6db5367a3049d8318efd30107e8685f93dfd17
tnthuy <thuytn@emotiv.vn>
2020-06-23 06:35:18 +0700

    qq

--- src/test/java/page/ContactUs.java
+++ src/test/java/page/ContactUs.java
@@ -2,6 +2,7 @@ package page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class ContactUs extends Base{
@@ -9,7 +10,7 @@ public class ContactUs extends Base{
 	public By DRL_SUBJECT=By.xpath("//*[@id=\"id_contact\"]");
 	public By TXT_EMAIL=By.xpath("//*[@id=\"email\"]");
 	public By TXT_ORDER=By.xpath("//*[@id=\"id_order\"]");
-	public By ATTACHFILE=By.xpath("//*[@class=\"action\"]");
+	public By ATTACHFILE=By.xpath("//*[@class=\"fileUpload\"]");
 	public By TXT_MESSAGE=By.xpath("//*[@id=\"message\"]");
 	public By BTN_SEND=By.xpath("//*[@id=\"submitMessage\"]");
 	public By MESSAGE=By.xpath("//*[@class=\"alert alert-success\"]");
@@ -24,14 +25,22 @@ public class ContactUs extends Base{
 		dropdown.selectByIndex(1);
 		driver.findElement(TXT_EMAIL).sendKeys("tnthuy140582@gmail.com");
 		driver.findElement(TXT_ORDER).sendKeys("Thuy Test");
-		driver.findElement(ATTACHFILE).sendKeys("C:\\Users\\Vinh\\Downloads\\tkb tuan31.pdf");
-		driver.findElement(TXT_MESSAGE).sendKeys(" Test add Contact Us for AutomationPractice");
+		WebElement uploadElement=driver.findElement(ATTACHFILE);
+		
+		// enter the file path onto the file-selection input field
+        uploadElement.sendKeys("C:\\Hello.java");
+        System.out.println("tim thay");
+        
+        driver.findElement(TXT_MESSAGE).sendKeys(" Test add Contact Us for AutomationPractice");
 		driver.findElement(BTN_SEND).click();
 		String mess=driver.findElement(MESSAGE).getText();
 		System.out.println(mess);
 		String message="Your message has been successfully sent to our team";
 		if (mess.equalsIgnoreCase(message)){
 			System.out.println("show dung message");
+		}
+		else {
+			System.out.println(" show chua dung "+ mess);
 		}
 		return new LoginGmail(driver);
 		
