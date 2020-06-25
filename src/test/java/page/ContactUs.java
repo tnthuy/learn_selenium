6086678d3527f3c76846647d88cb240e9fef634a
tnthuy <thuytn@emotiv.vn>
2020-06-25 22:28:22 +0700

    kk

--- src/test/java/page/ContactUs.java
+++ src/test/java/page/ContactUs.java
@@ -14,11 +14,8 @@ public class ContactUs extends Base{
 	public By DRL_SUBJECT=By.xpath("//*[@id=\"id_contact\"]");
 	public By TXT_EMAIL=By.xpath("//*[@id=\"email\"]");
 	public By TXT_ORDER=By.xpath("//*[@id=\"id_order\"]");
-<<<<<<< HEAD
-	public By ATTACHFILE=By.xpath("//*[@class=\"filename\"]");
-=======
+	//public By ATTACHFILE=By.xpath("//*[@class=\"filename\"]");
 	public By ATTACHFILE=By.xpath("//*[@id=\"fileUpload\"]");
->>>>>>> e9c854758babdb72d3bb1d5451036276b4a04724
 	public By TXT_MESSAGE=By.xpath("//*[@id=\"message\"]");
 	public By BTN_SEND=By.xpath("//*[@id=\"submitMessage\"]");
 	public By MESSAGE=By.xpath("//*[@class=\"alert alert-success\"]");
@@ -33,16 +30,11 @@ public class ContactUs extends Base{
 		dropdown.selectByIndex(1);
 		driver.findElement(TXT_EMAIL).sendKeys("tnthuy140582@gmail.com");
 		driver.findElement(TXT_ORDER).sendKeys("Thuy Test");
-<<<<<<< HEAD
-		WebElement uploadElement=driver.findElement(ATTACHFILE);
-		uploadElement.click();
-=======
-
-		//driver.findElement(ATTACHFILE).sendKeys("C:\\Users\\thuytn\\Downloads\\B52F099B-9219-4ED8-870C-808733FC5714.jpg");
+		
 		//To get current working directory: 
 		Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
 		System.out.println(path);
->>>>>>> e9c854758babdb72d3bb1d5451036276b4a04724
+
 		
 		//Locating the Test data excel file
 		String filePath =path.toString()+"\\B52F099B-9219-4ED8-870C-808733FC5714.jpg";
@@ -56,10 +48,10 @@ public class ContactUs extends Base{
 		System.out.println(mess);
 		String message="Your message has been successfully sent to our team";
 		if (mess.equalsIgnoreCase(message)){
-			System.out.println("show chua dung message: "+mess);
+			System.out.println("Show chua dung message: "+mess);
 		}
 		else {
-			System.out.println(" show dung message "+ mess);
+			System.out.println(" Show dung message: "+ mess);
 		}
 		return new LoginGmail(driver);
 
