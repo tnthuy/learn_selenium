fd08badd6232864cc77d4e13d409bba170d71792
tnthuy <thuy140583@gmail.com>
2020-06-25 15:26:30 +0700

    vv

--- src/test/java/page/ContactUs.java
+++ src/test/java/page/ContactUs.java
@@ -14,7 +14,7 @@ public class ContactUs extends Base{
 	public By DRL_SUBJECT=By.xpath("//*[@id=\"id_contact\"]");
 	public By TXT_EMAIL=By.xpath("//*[@id=\"email\"]");
 	public By TXT_ORDER=By.xpath("//*[@id=\"id_order\"]");
-	public By ATTACHFILE=By.xpath("//*[@id=\"uniform-fileUpload\"]");
+	public By ATTACHFILE=By.xpath("//*[@id=\"fileUpload\"]");
 	public By TXT_MESSAGE=By.xpath("//*[@id=\"message\"]");
 	public By BTN_SEND=By.xpath("//*[@id=\"submitMessage\"]");
 	public By MESSAGE=By.xpath("//*[@class=\"alert alert-success\"]");
@@ -23,27 +23,24 @@ public class ContactUs extends Base{
 	public ContactUs(WebDriver driver) {
 		this.driver=driver;
 	}
-	public LoginGmail contactUs() {
+	public LoginGmail contactUs() throws InterruptedException {
 		driver.findElement(BTN_CONTACTUS).click();
 		Select dropdown= new Select(driver.findElement(DRL_SUBJECT));
 		dropdown.selectByIndex(1);
 		driver.findElement(TXT_EMAIL).sendKeys("tnthuy140582@gmail.com");
 		driver.findElement(TXT_ORDER).sendKeys("Thuy Test");
-		WebElement uploadElement=driver.findElement(ATTACHFILE);
-	
-		uploadElement.click();
-		
-		//To get current working directory:
+
+		//driver.findElement(ATTACHFILE).sendKeys("C:\\Users\\thuytn\\Downloads\\B52F099B-9219-4ED8-870C-808733FC5714.jpg");
+		//To get current working directory: 
 		Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
 		System.out.println(path);
 		
 		//Locating the Test data excel file
-		String filePath =path.toString()+"\\Hello.java";
+		String filePath =path.toString()+"\\B52F099B-9219-4ED8-870C-808733FC5714.jpg";
+		System.out.println(filePath);
 
-
-		uploadElement.sendKeys(filePath);
-		System.out.println("tim thay");
-
+		driver.findElement(ATTACHFILE).sendKeys(filePath);
+		
 		driver.findElement(TXT_MESSAGE).sendKeys(" Test add Contact Us for AutomationPractice");
 		driver.findElement(BTN_SEND).click();
 		String mess=driver.findElement(MESSAGE).getText();
